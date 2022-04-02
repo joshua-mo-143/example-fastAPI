38ce84ebb678ca099b8cc8585d31b3e26a7ffedd
joshua-mo-143 <joshua.mo.876@gmail.com>
2022-04-02 21:33:40 +0100

    initial commit

---
+++ alembic/versions/c0ef425dab47_test.py
@@ -0,0 +1,24 @@
+"""test
+
+Revision ID: c0ef425dab47
+Revises: 
+Create Date: 2022-04-02 21:11:04.456132
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c0ef425dab47'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
